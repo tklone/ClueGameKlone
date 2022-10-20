db50e9487b307d9999928ec0ac37cd89d27c1976
smithqd <qsmith@mines.edu>
2022-10-20 10:35:26 -0600

    edit target test

--- src/tests/BoardAdjTargetTest.java
+++ src/tests/BoardAdjTargetTest.java
@@ -19,7 +19,7 @@ public class BoardAdjTargetTest {
 // Board is singleton, get the only instance
 		board = Board.getInstance();
 // set the file names to use my config files
-		board.setConfigFiles("ClueLayout.csv", "ClueSetup.txt");
+		board.setConfigFiles("ClueLayout306.csv", "ClueSetup306.txt");
 // Initialize will load config files 
 		board.initialize();
 	}
