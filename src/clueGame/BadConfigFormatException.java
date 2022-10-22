6a08a0af43d3e12bbd925d09e0f9c38b747596f4
smithqd <qsmith@mines.edu>
2022-10-22 13:00:51 -0600

    exception tests to pass

--- src/clueGame/BadConfigFormatException.java
+++ src/clueGame/BadConfigFormatException.java
@@ -1,9 +1,15 @@
 package clueGame;
 
-public class BadConfigFormatException extends Exception{
+import java.io.FileNotFoundException;
+
+public class BadConfigFormatException extends RuntimeException{
 
 	public BadConfigFormatException() {
-		// TODO Auto-generated constructor stub
+		super("File error");
+	}
+
+	public BadConfigFormatException(String invalidCell, String message) {
+		super("Invalid cell" + invalidCell + "contains error: " + message);
 	}
 
 }
