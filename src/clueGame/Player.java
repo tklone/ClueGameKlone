0964c69b03193547f5cef4b0dcd21c31c4e38a9c
Taylor Klone <tklone@mines.edu>
2022-10-31 13:35:21 -0600

    adding some more to players

--- src/clueGame/Player.java
+++ src/clueGame/Player.java
@@ -1,13 +1,31 @@
 package clueGame;
 
 import java.awt.Color;
+import java.util.ArrayList;
 
 public abstract class Player {
-	String name;
-	Color color;
-	int row, col;
+	private String name;
+	private Color color;
+	private int row, col;
+	private ArrayList <Card> hand = new ArrayList<>();
 	
 	public void updateHand(Card card) {
 		
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setColor(Color color) {
+		this.color = color;
+	}
+	
+	public Color getColor() {
+		return this.color;
 	}
 }
