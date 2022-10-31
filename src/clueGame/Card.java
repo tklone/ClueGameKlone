848ecbaa27308a5b4d8b3e2a96184c1435a8c380
Taylor Klone <tklone@mines.edu>
2022-10-31 13:30:30 -0600

    started on making the player classes

---
+++ src/clueGame/Card.java
@@ -0,0 +1,11 @@
+package clueGame;
+
+public class Card {
+	String cardName;
+	
+	
+	public Boolean equals(Card target) {
+		//Something should go in here
+		return null;
+	}
+}
---
+++ src/clueGame/Player.java
@@ -0,0 +1,13 @@
+package clueGame;
+
+import java.awt.Color;
+
+public abstract class Player {
+	String name;
+	Color color;
+	int row, col;
+	
+	public void updateHand(Card card) {
+		
+	}
+}
