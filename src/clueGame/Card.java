831e422bae67704552270c415f5bd74dfb4fceda
smithqd <qsmith@mines.edu>
2022-11-02 17:27:43 -0600

    setting card type to either person, weapon, or room in Card.java

--- src/clueGame/Card.java
+++ src/clueGame/Card.java
@@ -15,6 +15,17 @@ public class Card {
 	public void setCardType(CardType cardType) {
 		this.cardType = cardType;
 	}
+
+	/* Duplicate method but we will need to assign them like this somewhere
+	public void setCardType(String type) {
+		if (type.equals("Person")) {
+			cardType = clueGame.CardType.PERSON;
+		} else if (type.equals("Weapon")) {
+			cardType = clueGame.CardType.WEAPON;
+		} else if (type.equals("Room")) {
+			cardType = clueGame.CardType.ROOM;
+		}
+	}*/
 	
 	public CardType getCardType() {
 		return this.cardType;
