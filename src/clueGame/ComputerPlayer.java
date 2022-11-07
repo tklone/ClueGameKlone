93fd347ab560248380b4332b56c5ae4295aee982
Taylor Klone <tklone@mines.edu>
2022-11-07 16:59:08 -0700

    changed to not rand in createSuggestion

--- src/clueGame/ComputerPlayer.java
+++ src/clueGame/ComputerPlayer.java
@@ -12,25 +12,26 @@ public class ComputerPlayer extends Player {
 
 	
 	//It seems like this would be wrong because I don't know where we would call this
-	public Solution createSuggestion(ArrayList<Card> rooms, ArrayList<Card> people, ArrayList<Card> weapons) {
+	public Solution createSuggestion(Card room, Card person, Card weapon) {
 		Solution computerGuess = new Solution();
-		Random randRoomGuessNum = new Random();
-		Random randPersonGuessNum = new Random();
-		Random randWeaponGuessNum = new Random();
-
-		int upperBoundRooms = rooms.size() - 1;
-		int upperBoundPeople = people.size() - 1;
-		int upperBoundWeapons = weapons.size() - 1;
 		
-		
-		int randRooms = randRoomGuessNum.nextInt(upperBoundRooms);
-		int randPeople = randPersonGuessNum.nextInt(upperBoundPeople);
-		int randWeapons = randWeaponGuessNum.nextInt(upperBoundWeapons);
+//		Random randRoomGuessNum = new Random();
+//		Random randPersonGuessNum = new Random();
+//		Random randWeaponGuessNum = new Random();
+//
+//		int upperBoundRooms = room.size() - 1;
+//		int upperBoundPeople = person.size() - 1;
+//		int upperBoundWeapons = weapon.size() - 1;
+//		
+//		
+//		int randRooms = randRoomGuessNum.nextInt(upperBoundRooms);
+//		int randPeople = randPersonGuessNum.nextInt(upperBoundPeople);
+//		int randWeapons = randWeaponGuessNum.nextInt(upperBoundWeapons);
 
-		if (!hand.contains(rooms.get(randRooms)) && !hand.contains(people.get(randPeople)) && !hand.contains(weapons.get(randWeapons)) && !seenCards.contains(rooms.get(randRooms)) && !seenCards.contains(people.get(randPeople)) && !seenCards.contains(weapons.get(randWeapons))) {
-			computerGuess.setSolutionRoom(rooms.get(randRooms));
-			computerGuess.setSolutionPerson(people.get(randPeople));
-			computerGuess.setSolutionWeapon(weapons.get(randWeapons));
+		if (!hand.contains(room) && !hand.contains(person) && !hand.contains(weapon) && !seenCards.contains(room) && !seenCards.contains(person) && !seenCards.contains(weapon)) {
+			computerGuess.setSolutionRoom(room);
+			computerGuess.setSolutionPerson(person);
+			computerGuess.setSolutionWeapon(weapon);
 		}
 		
 		return computerGuess;
