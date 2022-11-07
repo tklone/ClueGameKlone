6e7e88df5ea8025e66dd07de9ca3ee047437d769
Taylor Klone <tklone@mines.edu>
2022-11-07 13:30:19 -0700

    made the JUnit test files

---
+++ src/tests/GameSolutionTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GameSolutionTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
