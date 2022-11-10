af207a49ad1792cbead5671b38fe28ca5816f4fa
smithqd <qsmith@mines.edu>
2022-11-10 11:01:14 -0700

    trying to get the sizing right

--- src/clueGame/GameControlPanel.java
+++ src/clueGame/GameControlPanel.java
@@ -11,7 +11,7 @@ import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 
 public class GameControlPanel extends JPanel {
-	
+
 	private static JTextField theGuess;
 	private static JTextField theGuessResult;
 	public static JButton nextButton = new JButton("NEXT!");
@@ -20,57 +20,57 @@ public class GameControlPanel extends JPanel {
 	/**
 	 * Constructor for the panel, it does 90% of the work
 	 */
-	public GameControlPanel()  {
-		
+	public GameControlPanel() {
+
 //		Display of the roll of the die
 //		Display of whose turn it is
 //		Display of guesses made by players and the result
 //		Be able to set or update the information in the fields with setters.
-		
-		setLayout(new GridLayout(1,1));
 
-		//GameControlPanel
+		//layout with 2 rows, 0 because we can change as we go
+		setLayout(new GridLayout(2, 0));
+
+		// GameControlPanel
 		JPanel gameControlPanel = new JPanel();
-		gameControlPanel.setLayout(new GridLayout(2,1));
-		
+		gameControlPanel.setLayout(new GridLayout(2, 1));
+
 		JPanel rightSide = new JPanel();
-		rightSide.setLayout(new GridLayout(1,4));
+		rightSide.setLayout(new GridLayout(1, 4));
 
 		// make accusation button
 		JPanel accusationButton = createAccusationButton();
 		rightSide.add(accusationButton, BorderLayout.WEST);
 
-		//Button to move to the next player
+		// Button to move to the next player
 		JPanel nextButton = createNextButton();
 		rightSide.add(nextButton, BorderLayout.EAST);
-		
+
 		gameControlPanel.add(rightSide, BorderLayout.NORTH);
 
-		
 		JPanel textFields = new JPanel();
-		textFields.setLayout(new GridLayout(1,2));
+		textFields.setLayout(new GridLayout(1, 2));
 
 		// theGuess text field
-		JPanel textField = createGuess();
-		textFields.add(textField, BorderLayout.WEST);
+		JPanel guessTextField = createGuess();
+		textFields.add(guessTextField, BorderLayout.WEST);
 
 		// theGuessResult text field
-		JPanel JPanel1x0x2 = createGuessResult();
-		textFields.add(JPanel1x0x2, BorderLayout.EAST);
+		JPanel guessResultTextField = createGuessResult();
+		textFields.add(guessResultTextField, BorderLayout.EAST);
 
 		gameControlPanel.add(textFields, BorderLayout.SOUTH);
 
 		add(gameControlPanel, BorderLayout.NORTH);
 	}
-	
+
 	// creates the Make Accusation button
-		private JPanel createAccusationButton() {
-			JPanel panel = new JPanel();
-			panel.setLayout(new GridLayout(1, 1));
-			panel.add(accusationButton);
-			return panel;
-		}
-		
+	private JPanel createAccusationButton() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(1, 1));
+		panel.add(accusationButton);
+		return panel;
+	}
+
 	// creates the NEXT! button
 	private JPanel createNextButton() {
 		JPanel panel = new JPanel();
@@ -78,14 +78,14 @@ public class GameControlPanel extends JPanel {
 		panel.add(nextButton);
 		return panel;
 	}
-	
-	//Make "Guess" text field
+
+	// Make "Guess" text field
 	private JPanel createGuess() {
 
 		JPanel panel = new JPanel();
 		panel.setLayout(new GridLayout(1, 1));
 
-		theGuess = new JTextField(10);
+		theGuess = new JTextField(5);
 		theGuess.setBorder(new TitledBorder(new EtchedBorder(), "Guess"));
 		panel.add(theGuess, BorderLayout.CENTER);
 
@@ -104,35 +104,34 @@ public class GameControlPanel extends JPanel {
 
 		return panel;
 	}
-	
+
 	/**
 	 * Main to test the panel
 	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		GameControlPanel panel = new GameControlPanel();  // create the panel
-		JFrame frame = new JFrame();  // create the frame 
+		GameControlPanel panel = new GameControlPanel(); // create the panel
+		JFrame frame = new JFrame(); // create the frame
 		frame.setContentPane(panel); // put the panel in the frame
-		frame.setSize(750, 180);  // size the frame
+		frame.setSize(750, 180); // size the frame
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // allow it to close
-		
+
 		frame.setVisible(true); // make it visible
-		
+
 		// test filling in the data
-		panel.setTurn(new ComputerPlayer( "Col. Mustard", "orange", 0, 0), 5);
-		panel.setGuess( "I have no guess!");
-		panel.setGuessResult( "So you have nothing?");
+		panel.setTurn(new ComputerPlayer("Col. Mustard", "orange", 0, 0), 5);
+		panel.setGuess("I have no guess!");
+		panel.setGuessResult("So you have nothing?");
 	}
 
-
-	//Setters to update data
+	// Setters to update data
 	public void setTurn(Player computerPlayer, int numSteps) {
-		
+
 	}
-	
+
 	public void setGuess(String guess) {
-	    theGuess.setText(guess);
+		theGuess.setText(guess);
 	}
 
 	public void setGuessResult(String guessResult) {
